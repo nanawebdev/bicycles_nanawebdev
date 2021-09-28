0285df5c11503ce8e6c20f5c2015b3d8eb41c959
nanawebdev <nanaprettysinger@gmail.com>
2021-09-28 18:51:24 +0300

    fix bugs from qa

--- source/js/main.js
+++ source/js/main.js
@@ -6,6 +6,21 @@
   var telInput = document.querySelector('input[type=tel]');
   var overlay = document.querySelector('.overlay');
   var mainPage = document.querySelector('.main-page');
+  var navLinks = document.querySelectorAll('.navigation-list__link');
+
+  var smoothLinks = document.querySelectorAll('a[href^="#"]');
+  for (var index = 0; index < smoothLinks.length; index++) {
+    var smoothLink = smoothLinks[index];
+    smoothLink.addEventListener('click', function (e) {
+      e.preventDefault();
+      var id = smoothLink.getAttribute('href');
+
+      document.querySelector(id).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    });
+  }
 
   function showNavList() {
     if (sideMenu) {
@@ -29,18 +44,31 @@
     }
   }
 
+  var form = document.querySelector('form');
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    validate();
+  });
+
+  var formWrapperAfter = document.querySelector('.form-section__form-wrapper');
+
   function validate() {
     if (/[a-z-а-я]/.test(telInput.value)) {
-      telInput.value = '';
+      formWrapperAfter.classList.add('form-section__form-wrapper--show-after');
       showErrorMessage();
     } else {
+      formWrapperAfter.classList.remove('form-section__form-wrapper--show-after');
       telInput.classList.remove('form__input--error');
     }
   }
 
+  navLinks.forEach(function (el) {
+    el.addEventListener('click', closeNavList);
+  });
+
   function showErrorMessage() {
     if (telInput) {
-      telInput.placeholder = 'Вводите цифры, пожалуйста';
       telInput.classList.add('form__input--error');
     }
   }
